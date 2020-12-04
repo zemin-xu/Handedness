5f23489580d8e16349c287cff4881b6b062ca66e
zemin xu <zemin.xu@yahoo.com>
2020-12-04 23:51:59 +0100

    csv done

---
+++ Assets/Scripts/CSVManager.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.IO;
+
+public static class CSVManager
+{
+    public const int FEATURE_NUM = 3;
+    private static string dataDirectoryName = "Data";
+    private static string dataFileName = "UserData.csv";
+    private static string seperator = ",";
+
+    private static string[] headers = new string[FEATURE_NUM + 1]
+    {
+        "position_x",
+        "position_y",
+        "position_z",
+        "left_handedness"
+    };
+
+    public static void Append(string[] strings)
+    {
+        VerifyDirectory();
+        VerifyFile();
+        using (StreamWriter sw = File.AppendText(GetFilePath()))
+        {
+            string res = "";
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (res != "")
+                {
+                    res += seperator;
+                }
+                res += strings[i];
+            }
+            sw.WriteLine(res);
+        }
+    }
+
+    public static void CreateData()
+    {
+        VerifyDirectory();
+        using (StreamWriter sw = File.CreateText(GetFilePath()))
+        {
+            string res = "";
+            for (int i = 0; i < headers.Length; i++)
+            {
+                if (res != "")
+                {
+                    res += seperator;
+                }
+                res += headers[i];
+            }
+            sw.WriteLine(res);
+        }
+    }
+
+    private static void VerifyDirectory()
+    {
+        string dir = GetDirectoryPath();
+        if (!Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+    }
+
+    private static void VerifyFile()
+    {
+        string file = GetFilePath();
+        if (!File.Exists(file))
+        {
+            return;
+        }
+    }
+
+    private static string GetDirectoryPath()
+    {
+        return Application.dataPath + "/" + dataDirectoryName;
+    }
+
+    private static string GetFilePath()
+    {
+        return GetDirectoryPath() + "/" + dataFileName;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public GameObject mouse;
+
+    private void Awake()
+    {
+        instance = this;
+
+        // reset data
+        CSVManager.CreateData();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            CSVManager.Append(GetData());
+            Debug.Log("Data updated");
+        }
+    }
+
+    private string[] GetData()
+    {
+        string[] ret = new string[CSVManager.FEATURE_NUM + 1];
+        ret[0] = mouse.transform.position.x.ToString();
+        ret[1] = mouse.transform.position.y.ToString();
+        ret[2] = mouse.transform.position.z.ToString();
+
+        ret[CSVManager.FEATURE_NUM] = 0.ToString();
+        return ret;
+    }
+}
