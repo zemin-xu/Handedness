e3b5acc4d17157e96c8cd480466e4064d2e43b04
zemin xu <zemin.xu@yahoo.com>
2020-12-05 13:14:35 +0100

    controller can move but with bugs

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// this script is attached on Controller to move Controller object by using mouse
+/// </summary>
+public class Controller : MonoBehaviour
+{
+    [SerializeField]
+    private Camera camera;
+
+    [SerializeField]
+    private float sensitivity = 0.01f;
+
+    private Vector3 prevPos;
+
+    private void Update()
+    {
+        Vector3 currPos = Input.mousePosition;
+        Vector3 offset = currPos - prevPos;
+
+        if (offset != Vector3.zero)
+        {
+            transform.rotation = camera.transform.rotation;
+
+            Vector3 dir = Vector3.ProjectOnPlane(offset, transform.forward).normalized;
+            transform.position = transform.position + offset.magnitude * dir * sensitivity;
+        }
+
+        prevPos = currPos;
+    }
+}
